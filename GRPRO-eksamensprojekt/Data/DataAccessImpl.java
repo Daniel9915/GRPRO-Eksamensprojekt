94540ca9dcdd47347b17375e7567591f5de0f472
Daniel9915 <69625109+Daniel9915@users.noreply.github.com>
2022-11-29 12:53:23 +0100

    Fixed data implements

--- GRPRO-eksamensprojekt/Data/DataAccessImpl.java
+++ GRPRO-eksamensprojekt/Data/DataAccessImpl.java
@@ -1,7 +1,12 @@
 package Data;
+import java.util.List;
 
 public class DataAccessImpl implements DataAccess{
-    public static void main(String[] args){
-        
+    public List<String> load(){
+        return null;
+    }
+
+    public List<String> save(){
+        return null;
     }
 }
