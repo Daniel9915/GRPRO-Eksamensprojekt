d193e696f638342bfab062417648973470c1de46
m4tt <mattiasehlert@gmail.com>
2022-12-12 20:36:37 +0100

    More gui added

--- GRPRO-eksamensprojekt/Presentation/MainPage.java
+++ GRPRO-eksamensprojekt/Presentation/MainPage.java
@@ -2,11 +2,12 @@ package Presentation;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class MainPage extends JFrame {
-
-    MainPage() {
+    MainPage(){
         //Creating the frame for GUI elements to exist in
         JFrame frame = new JFrame();
 
@@ -21,7 +22,6 @@ public class MainPage extends JFrame {
         frame.setResizable(false);
         frame.setSize(1000,1000);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //Makes application fullscreen
-        frame.setVisible(true);
 
         //////////////////
         //Frame contents//
@@ -32,61 +32,95 @@ public class MainPage extends JFrame {
         JPanel centerPanel = new JPanel();
         JPanel leftPanel = new JPanel();
         JPanel rightPanel = new JPanel();
-        
+
         topPanel.setBackground(new Color(55,55,55));
         centerPanel.setBackground(new Color(45,45,45));
         leftPanel.setBackground(new Color(65,65,65));
         rightPanel.setBackground(new Color(40,40,40));
-        
+
         topPanel.setPreferredSize(new Dimension(200,200));
         centerPanel.setPreferredSize(new Dimension(200,200));
         leftPanel.setPreferredSize(new Dimension(200,200));
         rightPanel.setPreferredSize(new Dimension(200,200));
-        
-        
+
         frame.add(topPanel,BorderLayout.NORTH);
         frame.add(centerPanel,BorderLayout.CENTER);
         frame.add(leftPanel,BorderLayout.WEST);
         frame.add(rightPanel,BorderLayout.EAST);
-        
-        
-        //BUTTONS
+
+        //PANELS
         JPanel topLeft = new JPanel();
         JPanel topCenter = new JPanel();
         JPanel topRight = new JPanel();
-        
+
         topPanel.setLayout(new GridLayout(1,3));
-        
+
         
         //TOP LEFT
-        JLabel icon = new JLabel("");
+        topLeft.setLayout(new FlowLayout(FlowLayout.CENTER, 10,100));
+        JLabel logoLabel = new JLabel("");
         ImageIcon logo = new ImageIcon("NETFLIX.png");
-        icon.setIcon(logo);
-        topLeft.add(icon);
-        
+        logoLabel.setIcon(logo);
+        topLeft.add(logoLabel);
+
         //TOP CENTER 
-        topCenter.setLayout(new FlowLayout(FlowLayout.CENTER,10,100));
-        JButton movieButt = new JButton("movie");
-        JButton serieButt = new JButton("movie");
-        JButton favoriteButt = new JButton("movie");        
-        movieButt.setBounds(100,100,100,100);
+
+        JPanel topCenterTop = new JPanel();
+        topCenterTop.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+
+        JPanel topCenterBot = new JPanel();
+        topCenterBot.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+
+        topCenter.setLayout(new GridLayout(2,1));
+
         
-        topCenter.add(movieButt);
-        topCenter.add(serieButt);
-        topCenter.add(favoriteButt);
+        //TOP CENTER TOP
+        JButton movieButt = new JButton("Movies");
+
+        movieButt.setPreferredSize(new Dimension(75,50));
+        movieButt.setBackground(new Color(50,50,50));
+        movieButt.setForeground(Color.red);
+
+        JButton serieButt = new JButton("Series");
+        serieButt.setBackground(new Color(50,50,50));
+        serieButt.setForeground(Color.red);
+
+        JButton favoriteButt = new JButton("Favorites");
+        favoriteButt.setBackground(new Color(50,50,50));
+        favoriteButt.setForeground(Color.red);
+
+        topCenterTop.add(movieButt);
+        topCenterTop.add(serieButt);
+        topCenterTop.add(favoriteButt);
+        topCenter.add(topCenterTop);
+
+        //TOP CENTER BOTTOM
+        String[] genres = {"Sort by", "Drama", "Romance", "Crime", "History", "Fantasy", "Family", "Adventure", "Mystery", "Thriller", "Horror", "Sci-fi", "Musical", "Comedy", "Biography", "War", "Action", "Western", "Film-Noir", "Talk-show", "Documentary", "Sport", "Animation"};
         
-        
-        
+        //combobox
+        JComboBox genreBox = new JComboBox(genres);
+        genreBox.setBackground(new Color(50,50,50));
+        genreBox.setForeground(Color.red);
+
+        //checkmarks
+        JCheckBox releaseCheck = new JCheckBox("Release date");
+        JCheckBox alphaCheck = new JCheckBox("Alphabetical");
+
+
+        topCenterBot.add(genreBox);
+        topCenterBot.add(releaseCheck);
+        topCenterBot.add(alphaCheck);
+        topCenter.add(topCenterBot);
+
+        //TOP RIGHT
         
         topPanel.add(topLeft);
         topPanel.add(topCenter);
         topPanel.add(topRight);
+
         
-        
-        
-        
-        
-        
+        frame.setVisible(true);
+
         //Title and logo
         /*JLabel title = new JLabel("");
         frame.add(title); //Required to make label visible
@@ -98,22 +132,18 @@ public class MainPage extends JFrame {
         title.setVerticalTextPosition(JLabel.TOP);
         title.setVerticalAlignment(JLabel.TOP);
         title.setHorizontalAlignment(JLabel.LEFT);
-        */
-       
+         */
+
         //Combo box/drop down list used for sorting media
         //String[][] movies = {}
         //JComboBox comboBox = new JComboBox();
 
         
-        
         //JButtons used to display all the media
-        
-    }
-    
-    
 
-        
-    
+    }
+
+
     
     
 }
